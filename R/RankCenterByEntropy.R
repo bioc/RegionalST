670892f091907e24391dfc94cce331c0dc23c833
Ziyi.Li <zli16@mdanderson.org>
2024-12-05 11:47:49 -0600

    fix bugs

--- R/RankCenterByEntropy.R
+++ R/RankCenterByEntropy.R
@@ -22,6 +22,11 @@
 #'                                 0.25,0.05,
 #'                                 0.025,0.05,
 #'                                 0.25,0.05,0.025))
+#' example_sce <- mySpatialPreprocess(example_sce, platform="Visium")
+#' example_sce$"array_col" <- example_sce$col
+#' example_sce$"array_row" <- example_sce$row
+#' example_sce$"pxl_col_in_fullres" <- example_sce$imagecol
+#' example_sce$"pxl_row_in_fullres" <- example_sce$imagerow
 #' example_sce <- RankCenterByEntropy(example_sce, weight, label = "celltype",
 #'                                   selectN = round(length(example_sce$spot)/10),
 #'                                   topN = 3, min_radius = 10,
